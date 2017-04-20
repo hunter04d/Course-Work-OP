2cd3cdfc0214b1718411363cd03dd2ef2c0d1963
hunter04d <hunter0d@gmail.com>
2017-04-20 17:19:19 +0300

    Init Commit

--- Course Work OP/Course Work OP.cpp
+++ Course Work OP/Course Work OP.cpp
@@ -1,11 +1,33 @@
 // Course Work OP.cpp : Defines the entry point for the console application.
 //
-
-#include "stdafx.h"
-
+#include "ShuntingYard.h"
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include "Maths.h"
+#include "Iterative.h"
 
 int main()
 {
-    return 0;
-}
+	std::string a = shuntingYard("x1^2+x2^2-1");
+	std::string b = shuntingYard("x1^3-x2");
+	std::cout << a << '\n';
+	std::cout << b << '\n';
+	std::vector<std::string> func = {a ,b };
+	std::vector<double> points = { 2,2 };
+	if (Iterative::mergeConditionBy_vNorm(func,points))
+	{
+		auto result = Iterative::getResult(func, points);
+		for (auto x : result)
+		{
+			std::cout << x << ' ';
+		}
+		std::cout << '\n';
+	}
+	else
+	{
+		std::cout << "does not merge\n";
+	}
 
+	system("pause");
+}
